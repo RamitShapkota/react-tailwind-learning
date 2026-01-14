5ef771356c19e3f5bf01e01a5209a876d8370e12
RamitShapkota <shapkotaramit47@gmail.com>
2026-01-14 07:48:07 +0545

    docs: explain how JSX works behind the scenes with createElement

--- Concept/02-react-JSX/src/App.jsx
+++ Concept/02-react-JSX/src/App.jsx
@@ -1,3 +1,21 @@
+import React from "react";
+
+// 1. Helper Component (JSX version)
+const Apps = () => {
+  return <h1>Hello, this is how we write jsx</h1>;
+};
+
+// 2. Helper Component (Behind the scenes version)
+const BehindTheScenes = () => {
+  return React.createElement("h1", null, "this is how jsx work behind the seen");
+};
+
+// 3. The Main Component (The one that gets displayed)
 export const App = () => {
-  return <h1>Hello, Ramit.I am from dhading</h1>
-}
+  return (
+    <>
+      <Apps />
+      <BehindTheScenes />
+    </>
+  );
+};
