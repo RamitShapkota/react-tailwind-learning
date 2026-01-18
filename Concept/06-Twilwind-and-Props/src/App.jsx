ac6b842f31d6aa655dee21d79d5197c30ba8b2c3
RamitShapkota <shapkotaramit47@gmail.com>
2026-01-18 23:02:47 +0545

    feat: install and configure tailwind css

---
+++ Concept/06-Twilwind-and-Props/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-amber-500 p-2 rounded-2xl hover:bg-amber-700 cursor-pointer'>Tailwind test</h1>
+      
+
+    </>
+  )
+}
+
+export default App
