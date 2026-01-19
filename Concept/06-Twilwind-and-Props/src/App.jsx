51df9b58e7576591343c0b9434a4a8afdc1d0089
RamitShapkota <shapkotaramit47@gmail.com>
2026-01-19 22:39:01 +0545

    feat: create reusable Card component using props

--- Concept/06-Twilwind-and-Props/src/App.jsx
+++ Concept/06-Twilwind-and-Props/src/App.jsx
@@ -1,15 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Card'
 
 function App() {
-
   return (
     <>
-      <h1 className='bg-amber-500 p-2 rounded-2xl hover:bg-amber-700 cursor-pointer'>Tailwind test</h1>
-      
+     <h1 className='bg-amber-500 p-2 px-6 rounded-2xl hover:bg-amber-700 cursor-pointer text-black font-bold mb-10'>
+        Tailwind test
+      </h1>
 
+      {/* <Card  myName = "Ramit" myboj = {{name : "Ramit" , age : 19 }}  myarr = {[1,2,3,4]}/> */}
+
+      <Card username = "Ramit Sonar" rank= "first" />
+      <Card username = "Dikshya Khanal" rank= "second"  />
+      <Card username = "Yasan Basnet"  />
+      
     </>
   )
 }
