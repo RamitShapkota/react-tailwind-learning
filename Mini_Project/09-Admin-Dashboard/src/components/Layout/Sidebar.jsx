3b4657dfbd1e396b9f0762e86c672e84484075de
RamitShapkota <shapkotaramit47@gmail.com>
2026-04-27 17:02:06 +0545

    feat(sidebar): add expandable menu system with dynamic page routing

--- Mini_Project/09-Admin-Dashboard/src/components/Layout/Sidebar.jsx
+++ Mini_Project/09-Admin-Dashboard/src/components/Layout/Sidebar.jsx
@@ -1,5 +1,5 @@
 import { BarChart3, Calendar, ChevronDown, CreditCard, FileText, LayoutDashboard, MessageSquare, Package, Settings, ShoppingBag, Users, Zap } from 'lucide-react'
-import React from 'react'
+import React, { useState } from 'react'
 
 const menuItem = [
     {
@@ -75,16 +75,33 @@ const menuItem = [
     },
 ]
 
-function Sidebar() {
+function Sidebar({collapsed,onToggle,currentPage,onPageChange}) {
+    const [expandedItems, setExpandedItems] = useState(new Set(["analytics"]));
+
+    const toggleExpand = (itemid) => {
+        const newExpanded = new Set(expandedItems);
+
+        if(newExpanded.has(itemid)){
+            newExpanded.delete(itemid);
+        }else{
+            newExpanded.add(itemid);
+        }
+
+        setExpandedItems(newExpanded);
+    }
+
     return (
-        <div className='w-72 transition duration-100 ease-in-out bg-white/80 dark:bg-slate-900/80 backdrop-blur-xl border-r border-slate-200/50 dark:border-slate-700/50 flex flex-col relative z-10'>
+        <div className={`${collapsed ? "w-20" : "w-72" } transition duration-100 ease-in-out bg-white/80 dark:bg-slate-900/80 backdrop-blur-xl border-r border-slate-200/50 dark:border-slate-700/50 flex flex-col relative z-10`}>
             {/* logo */}
             <div className='p-6 border-b border-slate-200/50 dark:border-slate-700/50 '>
                 <div className='flex items-center space-x-3'>
                     <div className='w-10 h-10 bg-gradient-to-r from-blue-600 to-purple-600 rounded-xl flex items-center justify-center shadow-lg'>
                         <Zap className='w-6 h-6 text-white' />
                     </div>
-                    <div >
+
+                    {/* conditional rendering */}
+                    {!collapsed && (
+                        <div >
                         <h1 className='text-xl font-bold text-slate-800 dark:text-white'>
                             Nexus
                         </h1>
@@ -92,6 +109,7 @@ function Sidebar() {
                             Admin panel
                         </p>
                     </div>
+                    )}
                 </div>
             </div>
 
@@ -101,15 +119,25 @@ function Sidebar() {
                 {menuItem.map((item) => {
                     return(
                         <div key={item.id}>
-                            <button className={`w-full flex items-center justify-between p-3 rounded-xl transition-all duration-200 dark:text-slate-300 dark:hover:bg-slate-800`}>
+                            <button className={`w-full flex items-center justify-between p-3 rounded-xl transition-all duration-200 ${currentPage === item.id || item.active ? "bg-gradient-to-r from-blue-500 to-purple-600 text-white shadow-lg shadow-blue-500/25": "text-slate-600 dark:text-slate-300 hover:bg-slate-100 dark:hover:bg-slate-800/50"}`}
+                            onClick={() => {
+                                if(item.submenu){
+                                    toggleExpand(item.id);
+                                }
+                                    onPageChange(item.id)
+                                
+                            }}
+                            >
                                 <div className='flex items-center space-x-3'>
                                     <item.icon className={`w-5 h-5`} />
 
                                     {/* conditional rendering */}
                                     <>
-                                    <span className='font-medium ml-2'>
+                                    {!collapsed && (
+                                           <span className='font-medium ml-2'>
                                         {item.label}
                                     </span>
+                                    )}
                                     {item.badge && (
                                         <span className='px-2 py-1 text-xs bg-red-500 text-white rounded-full'>{item.badge}</span>
                                     )}
@@ -119,12 +147,21 @@ function Sidebar() {
                                     </>
                                 </div>
 
-                                {item.submenu && (
+                                {!collapsed && item.submenu &&  (
                                     <ChevronDown className='w-4 h-4 transition-transform' />
                                 )}
                             </button>
 
                             {/* sub menus */}
+                            {!collapsed && item.submenu && expandedItems.has(item.id) && (
+                                <div className='ml-8 mt-2 space-y-1'>
+                                    {item.submenu.map((subitem) => {
+                                        return(
+                                            <button className='w-full text-left p-2 text-sm text-slate-600 dark:text-slate-400 hover:text-slate-800 dark:hover:text-slate-200 hober:bg-slate-100 dark:hover:bg-slate-800/50 rounded-lg transition-all'>{subitem.label}</button>
+                                        )
+                                    })}
+                                </div>
+                            )}
                             
                         </div>
                     )
@@ -133,7 +170,8 @@ function Sidebar() {
 
             {/* user profile */}
 
-            <div className='p-4 border-t border-slate-200/50 dark:border-slate-700/50'>
+            {!collapsed && (
+                <div className='p-4 border-t border-slate-200/50 dark:border-slate-700/50'>
                 <div className='flex items-center space-x-3 rounded-xl bg-slate-50 dark:bg-slate-800/50'>
                     <img
                         src="https://tse1.mm.bing.net/th/id/OIP.Q8yuUMqU_uigOHQ5DkeOtwHaE8?pid=Api&P=0&h=220"
@@ -152,6 +190,7 @@ function Sidebar() {
                     </div>
                 </div>
             </div>
+            )}
 
         </div>
     )
